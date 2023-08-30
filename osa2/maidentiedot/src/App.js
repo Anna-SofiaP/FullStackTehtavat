890aca29a3944291774d45bc1202a12503a37fc7
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2023-08-30 11:05:04 +0300

    osa2 - maidentiedot: ei etene...

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -2,34 +2,68 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [countries, setCountries] = useState(axios
-    .get('https://studies.cs.helsinki.fi/restcountries/api/all')
-    .then(response => console.log(response.data)))
+  const [countries, setCountries] = useState([])
   const [country, setCountry] = useState(null)
+  const [stateOfMatch, setStateOfMatch] = useState() //stateOfMatch voi olla 1, 2 tai 3
   const [filter, setFilter] = useState('')
+  const allCountries = axios
+    .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+    .then(response => console.log(response.data))
+  const alertText = "Too many matches, try a more specific filter"
 
   useEffect(() => {
+    const filteredCountries = []
+
     if (filter) {
-      console.log(countries)
-      
+      for (let i = 0; i < allCountries.length; i++) {
+        if (allCountries[i].name.common.toLowerCase().includes(filter.toLowerCase())) {
+          filteredCountries.push(i)
+          console.log(i)
+        }
+      }
     }
-    /*axios
-      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
-      .then(response => {
-        console.log(response)
-        console.log(response.data)
-        const newCountries = response.data
-        setCountries(newCountries)
-        const filteredCountries = newCountries.filter(country => 
-          country.name.common.toLowerCase().includes(filter.toLowerCase))
+
+    console.log(filteredCountries)
+
+    if (filteredCountries.length > 10) {
+      setStateOfMatch(1)
+    }
+
+    if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
+      setStateOfMatch(2)
+      const newCountries = filteredCountries.map(id => allCountries[id].name.common)
+      console.log(newCountries)
+    }
+
+    /*if (filter) {
+      axios
+        .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+        .then(response => {
+          console.log(response.data)
+          const newCountries = response.data
+          console.log(newCountries.length)
+          for (let i = 0; i < newCountries.length; i++) {
+            if (newCountries[i].name.common.toLowerCase().includes(filter.toLowerCase())) {
+              filteredCountries.push(i)
+              console.log(i)
+            }
+          }
       })
-    //const filteredCountries = newCountries.filter(country => country.common)*/
-  }, [])
+    }*/
+
+   // setCountries(filteredCountries)
+
+    //if(filteredCountries.length > 10) {
+
+    //}
+
+  }, [filter])
 
   const handleSearch = (event) => {
     event.preventDefault()
     const newFilter = event.target.value
     setFilter(newFilter)
+    console.log(newFilter)
   }
 
   return (
