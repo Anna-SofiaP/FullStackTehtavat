091258c68d72d3e4dfa1daf76b6a79d9ef2308b9
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2023-09-19 09:41:34 +0300

    nyt on valmis!

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,1 +1,20 @@
-//testiä
+import axios from 'axios'
+const url = '/api/persons'
+
+const getAll = async() => {
+  return await axios.get(url)
+}
+
+const create = async newObject => {
+  return await axios.post(url, newObject)
+}
+
+const remove = async id => {
+    return await axios.delete(`${url}/${id}`)
+}
+
+const update = async (id, updatedObject) => {
+    return await axios.put(`${url}/${id}`, updatedObject)
+}
+
+export default { getAll, create, remove, update }
